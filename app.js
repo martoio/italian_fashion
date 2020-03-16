ad25ffcb34b748ee1b774e4ef56b364640664cb4
Rostyslav <rklyba@yahoo.com>
2020-03-16 11:38:39 +0200

    Trying to use Express on my web site

--- app.js
+++ app.js
@@ -19,8 +19,15 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+//app.use('/', indexRouter);
+//app.use('/users', usersRouter);
+app.get('/', (req, res) => {
+  res.render('index')
+});
+
+app.get('/clothing', (req, res) => {
+  res.send('Clothing!')
+});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
