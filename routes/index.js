7e1f70d2d244c4612eb65e12c587d1dff8a214de
Rostyslav <rklyba@yahoo.com>
2020-03-17 21:38:50 +0200

    Added some routes

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,17 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', (req, res, next) => {
+  res.render('index', {title: 'Italian Fashion Shop. Main page'});
 });
 
-router.get('/men', function(req, res, next) {
-  res.render('men', {});
+router.get('/men', (req, res, next) => {
+  res.render('men', {title: "Italian Fashion Shop. Men's page"});
+});
+
+router.get('/children', (req, res, next) => {
+  res.render('children', {title: "Italian Fashion Shop. Children's page"});
 });
 
 module.exports = router;
--- routes/products.js
+++ routes/products.js
@@ -1,8 +1,22 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 
-/* GET products page. */
-router.get('/footwear', function(req, res, next) {
+// clothing //
+router.get('/clothing', (req, res, next) => {
+    const clothing = [
+    {
+      title: "Black Jeans",
+    },
+    {
+      title: "Armani Jeans",
+    },
+  ];
+  res.render('products', {products: clothing});
+});
+
+
+// footwear //
+router.get('/footwear', (req, res, next) => {
   const footwear = [
     {
       title: 'Blue shoes',
@@ -13,6 +27,45 @@ router.get('/footwear', function(req, res, next) {
     
   ];
   res.render('products', { products: footwear });
+});
+
+// accessories //
+router.get('/accessories', (req, res, next) => {
+  const accessories = [
+    {
+      title: 'Chanel bag'
+    },
+    {
+      title: 'Hermes bag'
+    }
+  ];
+  res.render('products', {products: accessories});
+});
+
+// underwear //
+router.get('/underwear', (req, res, next) => {
+  const underwear = [
+  {
+    title: 'Armani bra'
+  },
+  {
+    title: 'ST bra'
+  }
+  ];
+  res.render('products', {products: underwear});
+});
+
+// brands //
+router.get('/brands', (req, res, next) => {
+  const brands = [
+    {
+      title: "Hugo Boss"
+    },
+    {
+      title: "Bikkembergs"
+    }
+  ];
+  res.render('products', {products: brands});
 });
 
 module.exports = router;
